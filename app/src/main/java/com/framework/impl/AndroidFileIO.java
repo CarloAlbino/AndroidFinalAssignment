d2a023f7363048982940f06e9fc2cf1756279901
CarloAlbino <carloalbino@gmail.com>
2016-11-30 11:05:57 -0500

    added a temp char to the home screen

--- app/src/main/java/com/framework/impl/AndroidFileIO.java
+++ app/src/main/java/com/framework/impl/AndroidFileIO.java
@@ -19,6 +19,7 @@ public class AndroidFileIO implements FileIO {
     public AndroidFileIO(AssetManager assets) {
         this.assets = assets;
         this.externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+        System.out.println(externalStoragePath);
     }
 
     @Override
